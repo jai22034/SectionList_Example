a06d1845ef4a9258de517ee96a7e6e5b9c0b04f6
j2itu <jai22034@gmail.com>
2019-07-15 13:57:42 +0530

    section list example 27/06/2019

---
+++ Sectionlist/App.js
@@ -0,0 +1,66 @@
+
+
+import React, {Component} from 'react';
+import {StyleSheet, View, SectionList, Text, Platform, Alert} from 'react-native';
+
+
+
+
+export default class App extends Component {
+  GetSectionListItem=(item)=>{
+ 
+    Alert.alert(item)
+ 
+  }
+  render() {
+    var A = ['Apple', 'Apricot'] ;
+    var B = ['Banana', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry'] ;
+    var C = ['Cherry', 'Coconut'] ;
+ 
+    return (
+ 
+      <View style={{ marginTop : (Platform.OS) == 'ios' ? 20 : 0 }}>
+ 
+      <SectionList
+ 
+          sections={[
+ 
+            { title: 'Fruits Name From A', data: A },
+ 
+            { title: 'Fruits Name From B', data: B },
+ 
+            { title: 'Fruits Name From C', data: C },
+ 
+          ]}
+ 
+          renderSectionHeader={ ({section}) => <Text style={styles.SectionHeaderStyle}> { section.title } </Text> }
+ 
+          renderItem={ ({item}) => <Text style={styles.SectionListItemStyle} onPress={this.GetSectionListItem.bind(this, item)}> { item } </Text> }
+ 
+          keyExtractor={ (item, index) => index }
+         
+        />
+ 
+      </View> 
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  SectionHeaderStyle:{
+ 
+    backgroundColor : '#CDDC39',
+    fontSize : 20,
+    padding: 5,
+    color: '#fff',
+  },
+ 
+  SectionListItemStyle:{
+ 
+    fontSize : 15,
+    padding: 5,
+    color: '#000',
+    backgroundColor : '#F5F5F5'
+ 
+  }
+});
